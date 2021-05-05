01de2a271f614aa5524337305f3fac0557c300ab
Marcelo-Biker <marcelo-biker@hotmail.com>
2021-05-05 21:39:55 +0100

    project initiated

---
+++ backend/src/main/java/com/devsuperior/sds3project/Sds3ProjectApplication.java
@@ -0,0 +1,13 @@
+package com.devsuperior.sds3project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sds3ProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sds3ProjectApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/devsuperior/sds3project/Sds3ProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperior.sds3project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sds3ProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
