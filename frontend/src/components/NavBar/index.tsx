3071013da2a7c4da717cb04a080ec4fdb39f0a45
Marcelo-Biker-Dev <marcelo.biker.dev@gmail.com>
2021-05-09 22:22:10 +0100

    Home, navigation links

--- frontend/src/components/NavBar/index.tsx
+++ frontend/src/components/NavBar/index.tsx
@@ -1,11 +1,14 @@
 import img_CHAMBERTRAN from "assets/img/Logo_CHAMBERTRAN.png"
+import { Link } from "react-router-dom";
 
 function NavBar() {
     return (
         <div className="d-flex flex-column flex-md-row align-items-center p-1 px-md-4 mb-3 bg-0 border-bottom shadow-sm">
             <div className="container">
                 <nav className="my-2 my-md-0 mr-md-3">
-                    <img src={img_CHAMBERTRAN} alt="Chambertran" width="500" />
+                    <Link to="/">
+                        <img src={img_CHAMBERTRAN} alt="Chambertran" width="500" />
+                    </Link>
                 </nav>
             </div>
         </div>
