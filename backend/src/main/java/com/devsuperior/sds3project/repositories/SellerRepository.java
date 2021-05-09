6be41f04596249a0009955925ccf3e62c882543a
Marcelo-Biker-Dev <marcelo.biker.dev@gmail.com>
2021-05-09 01:30:24 +0100

    layers

---
+++ backend/src/main/java/com/devsuperior/sds3project/repositories/SellerRepository.java
@@ -0,0 +1,10 @@
+package com.devsuperior.sds3project.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.devsuperior.sds3project.entities.Seller;
+
+public interface SellerRepository extends JpaRepository<Seller, Long>{
+	
+
+}
