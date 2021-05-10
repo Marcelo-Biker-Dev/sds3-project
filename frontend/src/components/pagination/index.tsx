e1376ebc3e8b2b7496263fcca5e62acc1b544a81
Marcelo-Biker-Dev <marcelo.biker.dev@gmail.com>
2021-05-10 05:15:33 +0100

    pagination final

--- frontend/src/components/pagination/index.tsx
+++ frontend/src/components/pagination/index.tsx
@@ -1,15 +1,28 @@
-<div className="row d-flex justify-content-center">
-    <nav>
-        <ul className="pagination">
-            <li className="page-item">
-                <button className="page-link">Anterior</button>
-            </li>
-            <li className="page-item disabled">
-                <span className="page-link">1</span>
-            </li>
-            <li className="page-item disabled">
-                <button className="page-link">Próxima</button>
-            </li>
-        </ul>
-    </nav>
-</div>
+import { SalePage } from "types/sale";
+
+type Props = {
+    page: SalePage;
+    onPageChange: Function;
+}
+
+const Pagination = ( { page, onPageChange } : Props) => {
+    return (
+        <div className="row d-flex justify-content-center">
+            <nav>
+                <ul className="pagination">
+                    <li className={`page-item ${page.first ? "disabled" : ""}`}>
+                        <button className="page-link" onClick={() => onPageChange(page.number - 1) }>Preview</button>
+                    </li>
+                    <li className="page-item disabled">
+                        <span className="page-link">{page.number + 1}</span>
+                    </li>
+                    <li className={`page-item ${page.last ? "disabled" : ""}`}>
+                        <button className="page-link" onClick={() => onPageChange(page.number + 1) }>Next</button>
+                    </li>
+                </ul>
+            </nav>
+        </div>
+    )
+}
+
+export default Pagination;
